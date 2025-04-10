e08d36dbcdd014724c012babb428eff99c0d630e
llayes <122802140+llayes@users.noreply.github.com>
2025-04-10 18:19:22 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,24 +1,29 @@
 from cryptography.fernet import Fernet
-from flask import Flask, render_template_string, render_template, jsonify
-from flask import render_template
-from flask import json
-from urllib.request import urlopen
-import sqlite3
-                                                                                                                                       
-app = Flask(__name__)                                                                                                                  
-                                                                                                                                       
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Génération de la clé de chiffrement
+key = Fernet.generate_key()
+f = Fernet(key)
+
 @app.route('/')
 def hello_world():
     return render_template('hello.html')
 
-key = Fernet.generate_key()
-f = Fernet(key)
-
+# Route pour encrypter une valeur
 @app.route('/encrypt/<string:valeur>')
 def encryptage(valeur):
     valeur_bytes = valeur.encode()  # Conversion str -> bytes
     token = f.encrypt(valeur_bytes)  # Encrypt la valeur
     return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
-                                                                                                                                                     
+
+# Route pour décrypter une valeur
+@app.route('/decrypt/<string:token>')
+def decryptage(token):
+    token_bytes = token.encode()  # Conversion str -> bytes
+    valeur = f.decrypt(token_bytes)  # Decrypt le token
+    return f"Valeur décryptée : {valeur.decode()}"  # Retourne la valeur en str
+
 if __name__ == "__main__":
-  app.run(debug=True)
+    app.run(debug=True)
