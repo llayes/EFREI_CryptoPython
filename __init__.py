bedc25d39aef63f3a0a51f7c404403c4ef34b320
llayes <122802140+llayes@users.noreply.github.com>
2025-04-10 18:27:35 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,27 +3,27 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-# Génération de la clé de chiffrement
+# Génération de la clé (valable pour la session du serveur)
 key = Fernet.generate_key()
 f = Fernet(key)
 
 @app.route('/')
 def hello_world():
-    return render_template('hello.html')
+    return render_template('hello.html')  #commit
 
-# Route pour encrypter une valeur
+# Route Encrypt : chiffre une valeur
 @app.route('/encrypt/<string:valeur>')
 def encryptage(valeur):
-    valeur_bytes = valeur.encode()  # Conversion str -> bytes
-    token = f.encrypt(valeur_bytes)  # Encrypt la valeur
-    return f"Valeur encryptée : {token.decode()}"  # Retourne le token en str
+    valeur_bytes = valeur.encode()  # str -> bytes
+    token = f.encrypt(valeur_bytes)  # Encrypt
+    return f"Valeur encryptée : {token.decode()}"  # bytes -> str
 
-# Route pour décrypter une valeur
+# Route Decrypt : déchiffre un token
 @app.route('/decrypt/<string:token>')
 def decryptage(token):
-    token_bytes = token.encode()  # Conversion str -> bytes
-    valeur = f.decrypt(token_bytes)  # Decrypt le token
-    return f"Valeur décryptée : {valeur.decode()}"  # Retourne la valeur en str
+    token_bytes = token.encode()  # str -> bytes
+    valeur = f.decrypt(token_bytes)  # Decrypt
+    return f"Valeur décryptée : {valeur.decode()}"  # bytes -> str
 
 if __name__ == "__main__":
     app.run(debug=True)
